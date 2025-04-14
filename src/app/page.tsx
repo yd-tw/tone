dc0d413292b8e7d1e916996e66330b317965798b
yd-tw <151503280+yd-tw@users.noreply.github.com>
2025-04-14 12:52:22 +0800

    更新 page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
     transport.stop();
     transport.cancel();
     transport.timeSignature = [6, 8];
-    transport.bpm.value = 60;
+    transport.bpm.value = 75;
 
     let currentTime = 0;
 
